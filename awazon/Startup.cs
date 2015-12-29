0931d833dc8c65cd978ea5f6d9c694a969f1b11f
mizunowanko <siyou1315@gmail.com>
2015-12-29 17:36:51 +0900

    初コミット

---
+++ awazon/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(awazon.Startup))]
+namespace awazon
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
